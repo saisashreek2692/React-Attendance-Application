fcd8a5d1aff369b66b31078dba41903c7010b81d
Sai Sashreek D <saisashreekd@gmail.com>
2024-06-10 15:48:57 +0530

    Font Size Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 function App() {
   return (
     <>
-      <h1 className="text-center py-6 text-emerald-600 font-Jost font-medium">
+      <h1 className="text-center py-6 text-emerald-600 font-Jost font-medium text-3xl">
         Hello World
       </h1>
-      <p className="text-justify py-3 text-cyan-400 font-Jost font-normal">
+      <p className="text-justify py-3 text-cyan-400 font-Jost font-normal text-lg">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque ducimus
         hic autem atque commodi quaerat, voluptas ab aut dignissimos mollitia
         tempore quasi fugiat a accusamus porro dolore, fuga ipsam ad.
