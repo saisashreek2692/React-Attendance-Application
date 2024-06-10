5d12c2a7cad71205ffe051adee96529107a3dae8
Sai Sashreek D <saisashreekd@gmail.com>
2024-06-10 14:37:18 +0530

    New Tailwind CSS Packages Added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Jost: ["Jost", "sans-serif"],
+    },
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
