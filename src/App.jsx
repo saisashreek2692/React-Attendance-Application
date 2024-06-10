5f1376e33be8ea366b5915119d91b89106863810
Sai Sashreek D <saisashreekd@gmail.com>
2024-06-10 14:37:31 +0530

    Added Hello Text

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1 className="text-center py-6 text-emerald-600 font-Jost font-medium">
+        Hello World
+      </h1>
+      <p className="text-justify py-3 text-cyan-400 font-Jost font-normal">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque ducimus
+        hic autem atque commodi quaerat, voluptas ab aut dignissimos mollitia
+        tempore quasi fugiat a accusamus porro dolore, fuga ipsam ad.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
